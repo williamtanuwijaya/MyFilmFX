8f10d0fe0ceeeb839aaf1b204f2c7baaca4e1b60
williamtanuwijaya <will.tw0807@gmail.com>
2023-11-11 14:04:38 +0700

    rename login.jsx to Login.jsx

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 // import 'swiper/swiper.min.css';
 // import "./assets/boxicons-2.0.7/css/boxicons.min.css";
-import "./App.scss";
-import React, { useState } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import HalamanLogin from "./pages/login/Login";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import Routes from "./config/Routes";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-
+import './App.scss';
+import React, { useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Routes from './config/Routes';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 function App() {
-
   const [currentForm, setCurrentForm] = useState('');
 
-  if(currentForm === "Login"){
-    return <Login/>
-  }
-
-  else if(currentForm === "Register"){
-    return <Register/>
-  }
-
-  else{
+  if (currentForm === 'Login') {
+    return <Login />;
+  } else if (currentForm === 'Register') {
+    return <Register />;
+  } else {
     return (
       <BrowserRouter>
         <Route
@@ -38,10 +31,6 @@ function App() {
       </BrowserRouter>
     );
   }
-
-  
-   
-
 }
 
 export default App;
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,73 +1,54 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import logo from "../../assets/tmovie.png";
-import "./login.css";
+import React, { useState } from 'react';
+import logo from '../../assets/tmovie.png';
+import './login.css';
 
 const Login = () => {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
     console.log(`Username : ${username}`);
     console.log(`Password : ${password}`);
   }
 
   return (
-    
-      <div className="containerInduk">
-        <div className="containerHeader">
-          <img src={logo} alt="" className="logo" />
-          <h2>MyFilmFx</h2>
+    <div className="containerInduk">
+      <div className="containerHeader">
+        <img src={logo} alt="" className="logo" />
+        <h2>MyFilmFx</h2>
+      </div>
+
+      <form onSubmit={handleSubmit} className="form_login">
+        <div className="container_login">
+          <div className="judul">
+            <h1>Log In</h1>
+          </div>
+
+          <hr />
+          <label htmlFor="email">
+            <b>Username</b>
+          </label>
+          <input type="text" placeholder="Enter Username" name="email" id="email" value={username} onChange={(e) => setUsername(e.target.value)} required></input>
+
+          <label htmlFor="psw">
+            <b>Password</b>
+          </label>
+          <input type="password" placeholder="Enter Password" name="psw" id="psw" value={password} onChange={(e) => setPassword(e.target.value)} required></input>
+
+          <hr />
+          <button type="submit" className="registerbtn">
+            Login
+          </button>
         </div>
 
-        <form onSubmit={handleSubmit} className="form_login">
-          <div className="container_login">
-            <div className="judul">
-              <h1>Log In</h1>
-            </div>
-
-            <hr />
-            <label htmlFor="email">
-              <b>Username</b>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter Username"
-              name="email"
-              id="email"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            ></input>
-
-            <label htmlFor="psw">
-              <b>Password</b>
-            </label>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              name="psw"
-              id="psw"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            ></input>
-
-            <hr />
-            <button type="submit" className="registerbtn">
-              Login
-            </button>
-          </div>
-
-          <div className="container signin">
-            <p>
-              Belum Memiliki Akun? <a href="/register">Register</a>.
-            </p>
-          </div>
-        </form>
-      </div>
-    
+        <div className="container signin">
+          <p>
+            Belum Memiliki Akun? <a href="/register">Register</a>.
+          </p>
+        </div>
+      </form>
+    </div>
   );
 };
 
