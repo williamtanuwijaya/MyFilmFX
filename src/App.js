7299de5c309d9c524df0ba1c1224c27c5663820d
zaki <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2023-11-15 09:17:25 +0700

    Menambahkan logic tampilan login di file App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-// import 'swiper/swiper.min.css';
-// import "./assets/boxicons-2.0.7/css/boxicons.min.css";
-import './App.scss';
-import React, { useState } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Routes from './config/Routes';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./App.scss";
+
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Routes from "./config/Routes";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  if (currentForm === 'Login') {
-    return <Login />;
-  } else if (currentForm === 'Register') {
-    return <Register />;
-  } else {
-    return (
-      <BrowserRouter>
-        <Route
-          render={(props) => (
-            <>
-              <Header {...props} />
-              <Routes />
-              <Footer />
-            </>
-          )}
-        />
-      </BrowserRouter>
-    );
-  }
+  useEffect(() => {
+    const checkLoginStatus = () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(true);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      {isLoggedIn ? (
+        <>
+          <Header />
+          <Routes />
+          <Footer />
+        </>
+      ) : (
+        <Login />
+      )}
+    </BrowserRouter>
+  );
 }
 
 export default App;
